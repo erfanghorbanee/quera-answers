67c01adabba45f315dcc11e7c7d87d662f654464
alireza ghorbani <49264993+erfanghorbanee@users.noreply.github.com>
2021-02-06 01:46:04 +0330

    Add files via upload

---
+++ مسابقه/مجید، میلاد، رشته‌سازی/temp.py
@@ -0,0 +1,22 @@
+# Returns '0' for '1' and '1' for '0' 
+def flip(c): 
+	return '1' if (c == '0') else '0'
+
+
+def One_Complement(bin): 
+
+	n = len(bin) 
+	ones = "" 
+
+	for i in range(n): 
+		ones += flip(bin[i]) 
+
+	return ones  
+	
+
+l , r = input().split(" ")
+b = "1"
+for i in range(17):
+    b += One_Complement(b)
+
+print(b[int(l)-1 : int(r)]) 
