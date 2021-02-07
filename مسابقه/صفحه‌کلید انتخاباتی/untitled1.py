dbd1b6474fcd81bed8c8cda15d113317b44b7327
alireza ghorbani <49264993+erfanghorbanee@users.noreply.github.com>
2021-02-07 10:29:54 +0330

    Add files via upload

---
+++ مسابقه/صفحه‌کلید انتخاباتی/untitled1.py
@@ -0,0 +1,16 @@
+n = int(input())
+string = ""
+caps = -1 #CapsLock off
+
+for i in range(n):
+    inp = input()
+    if inp == "CAPS":
+        caps = - caps
+        inp = ""
+        
+    if caps == 1: #CapsLock on    
+        string += inp.upper()
+    elif caps == -1:
+        string += inp
+
+print(string)
