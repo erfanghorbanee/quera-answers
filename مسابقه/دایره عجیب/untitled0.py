5b3fbd188fe80d2a16bf8f06bb248fb523caea62
alireza ghorbani <49264993+erfanghorbanee@users.noreply.github.com>
2021-02-10 01:28:08 +0330

    Add files via upload

---
+++ مسابقه/دایره عجیب/untitled0.py
@@ -0,0 +1,24 @@
+n, k = input().split(" ")
+n, k = int(n), int(k)
+
+s = []
+for i in range(1, n+1):
+    s.append(i)
+
+i = 0
+rounds = 0
+while(True):
+    i += k
+    rounds += 1
+    
+    if i <= n-1:
+        if s[i] == 1:
+            # print(i)
+            break
+        
+    elif i > n-1:
+        i = i - (n)
+        if s[i] == 1:
+            break
+
+print(rounds)
