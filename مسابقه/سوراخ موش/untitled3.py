7eab8da41eafa4b482c9c6bd352d903da6ab8b60
alireza ghorbani <49264993+erfanghorbanee@users.noreply.github.com>
2021-03-15 17:35:03 +0330

    Add files via upload

---
+++ مسابقه/سوراخ موش/untitled3.py
@@ -0,0 +1,10 @@
+x1, x2 = input().split(" ")
+x1 = int(x1)
+x2 = int(x2)
+
+if x1 == x2:
+    print("Saal Noo Mobarak!")
+elif x1 < x2:
+    print((x2-x1)*"R")
+elif x1 > x2:
+    print((x1-x2)*"L")
